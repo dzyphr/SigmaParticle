1c75b65aa28f49596cf195f633bdd757366ca712
justaguy12345 <ryanberk135@gmail.com>
2023-02-09 01:33:13 -0600

    updated deploy.py and gitignore

--- basic_framework/py/deploy.py
+++ basic_framework/py/deploy.py
@@ -4,10 +4,10 @@ loggingFmt()
 
 # Create connection to the blockchain #exported to connect.py
 from connect import *
-ergo, wallet_mnemonic, mnemonic_password, senderAddress = connect() #dotenv loaded here dont call env vars before
+ergo, wallet_mnemonic, mnemonic_password, senderAddress,  ak = connect() #dotenv loaded here dont call env vars before
 
 from main import *
-main(os.getenv('ContractName'), ergo, wallet_mnemonic, mnemonic_password, senderAddress)
+main(os.getenv('ContractName'), ergo, wallet_mnemonic, mnemonic_password, senderAddress, ak)
 
 from cleanup import *
 cleanup()
