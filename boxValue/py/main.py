5958496e9dee182ffe82bcd30500b26d01ae10e7
justaguy12345 <ryanberk135@gmail.com>
2023-05-15 20:52:01 -0500

    add filepath option to boxVal function

--- boxValue/py/main.py
+++ boxValue/py/main.py
@@ -13,12 +13,24 @@ import coinSelection
 import scalaPipe
 def main(contractName, ergo, wallet_mnemonic, mnemonic_password, senderAddress, args):
  
-    def boxVal(boxId):
-        inputBox = java.util.Arrays.asList(ergo._ctx.getBoxesById(boxId)) 
-        sys.stdout.write(str(inputBox[0].getValue()))
+    def boxVal(boxId, filepath=None):
+        if filepath == None:
+            inputBox = java.util.Arrays.asList(ergo._ctx.getBoxesById(boxId))
+    #       sys.stdout.write(str(inputBox[0].getValue()))
+            print(str(inputBox[0].getValue()))
+        else:
+            value = java.util.Arrays.asList(ergo._ctx.getBoxesById(boxId))[0].getValue()
+            print(value)
+            f = open(filepath, "w")
+            f.write(str(value))
+            f.close()
 
     if len(args) > 1:
+        if len(args) > 2:
+            boxVal(args[1], args[2])
+            exit()
         boxVal(args[1])
+        exit()
+
     else:
         print("enter boxId as argument")
-
